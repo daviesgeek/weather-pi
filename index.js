0923544b8dd6815647f32814d7517d9d58e339b6
Matthew Davies <daviesgeek@icloud.com>
2015-11-07 20:43:07 -0800

    Added socket.io and got really basic frontend working

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-var express = require("express");
-var server = express();
-var serverPort = 3000;
+var express = require('express')
+    app = express(),
+    serverPort = 3000,
+    thinky = require('thinky')({host: 'rethinkdb.local', port: '49186', db: 'database'})
+    type = thinky.type,
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
-server.get('/', function(req, res) {
-  res.send("Work in progress...")
+var Data = thinky.createModel('Data', {
+  id: type.string(),
+  date: type.date(),
+  temp: type.number(),
+  humidity: type.number(),
 })
 
-server.listen(serverPort, function() {});
+var stringify = function(doc) {
+  return JSON.stringify(doc, null, 2);
+}
+
+io.on('connection', function(socket){
+  var data = Data.orderBy('date').run().then(function (data) {
+    io.emit('data', data)
+    Data.changes().then(function (feed) {
+      var newData = []
+      feed.each(function (error, doc) {
+        io.emit('newData', doc)
+      })
+    })
+  })
+})
+
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('./public/index.html')
+})
+
+app.get('/new', function(req, res) {
+  var row = new Data({temp: 75, humidity: 20, date: new Date()})
+  row.save()
+  res.send(row)
+})
+
+http.listen(serverPort, function() {});
---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+angular.module('app', ['btford.socket-io'])
+  .factory('db', function(socketFactory){
+    return socketFactory()
+  })
+  .controller('MainCtrl', function($scope, db) {
+    db.emit('connection', 'test')
+    db.on('data', function (data) {
+      $scope.data = data
+    })
+    db.on('newData', function (newData) {
+      $scope.data = $scope.data.concat(newData)
+    })
+  })
